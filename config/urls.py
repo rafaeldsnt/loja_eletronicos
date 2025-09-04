1605e0220b66be318e671e8fb8e0b49ab33df2ba
Rafael Santos <rafaeldsnt@hotmail.com>
2025-09-03 21:03:03 -0300

    first commit

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+# Adicione estas importações para servir arquivos de mídia em desenvolvimento (Aula 10)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('site-admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ loja/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Perfil, Comentario
+
+admin.site.register(Perfil)
+
+admin.site.register(Comentario)
+
+
+#adminuser #12345678
