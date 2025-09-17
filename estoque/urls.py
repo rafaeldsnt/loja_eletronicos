848c72ebb214b175747d0155ba069914b0210fd1
Rafael Santos <rafaeldsnt@hotmail.com>
2025-09-17 20:46:19 -0300

    aula 2

--- estoque/urls.py
+++ estoque/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'estoque'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('produtos/listar/', views.ProdutoTabelaListView.as_view(), name='listprod'),
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -3,11 +3,18 @@ from django.urls import reverse_lazy
 from django.views.generic import (ListView, DeleteView,CreateView,
 UpdateView,DetailView)
 from django.contrib import messages
+from .models import Produto
 
 def home(request):
-
     context = {
         'titulo' : "Bem-Vindo ao site !!!!!"
     }
 
     return render(request, 'home.html', context)
+
+
+class ProdutoTabelaListView(ListView):
+    model = Produto
+    template_name = 'estoque/produto_tabela_list.html' 
+    context_object_name = 'produtos'
+    ordering = ['nome']
